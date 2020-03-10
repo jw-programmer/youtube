03a5b9cf13a67a5f7ab4b2931344e804b151c8ce
Josias Wando Nobre de Queiroz <josiasn.queiroz@gmail.com>
2020-03-09 22:31:36 -0300

    Navegação pela barra de baixo concluida

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/telas/Biblioteca.dart';
+import 'package:youtube/telas/EmAlta.dart';
+import 'package:youtube/telas/Incricao.dart';
+import 'package:youtube/telas/Inicio.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,8 +10,15 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _indiceBottonAtual = 0;
   @override
   Widget build(BuildContext context) {
+    List<Widget> telas = [
+      Inicio(),
+      Inscricao(),
+      EmAlta(),
+      Biblioteca()
+    ];
     return Scaffold(
       appBar: AppBar(
         title: Image.asset(
@@ -37,7 +48,31 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
-      body: Container(),
+      body: telas[_indiceBottonAtual],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _indiceBottonAtual,
+        onTap: (indice) {
+          setState(() {
+            _indiceBottonAtual = indice;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.red,
+        items: [
+          BottomNavigationBarItem(
+              title: Text("Inicio"), icon: Icon(Icons.home)),
+          BottomNavigationBarItem(
+              title: Text("Em alta"), icon: Icon(Icons.whatshot)),
+          BottomNavigationBarItem(
+              title: Text("Inscrições"),
+//              backgroundColor: Colors.blue,
+              icon: Icon(Icons.subscriptions)),
+          BottomNavigationBarItem(
+              title: Text("Biblioteca"),
+//              backgroundColor: Colors.green,
+              icon: Icon(Icons.folder)),
+        ],
+      ),
     );
   }
 }
